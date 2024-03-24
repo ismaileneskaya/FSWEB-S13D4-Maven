8329c264a49729ced9cd08278f815fb6bd6730eb
ismaileneskaya@gmail.com <ismaileneskaya@gmail.com>
2024-03-24 17:19:38 +0300

    Completed...

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,7 +1,34 @@
 package org.example;
 
 public class Player {
-    String name;
-    int healthPercentage;
-    Weapon weapon;
+   private String name;
+   private int healthPercentage;
+
+   private Weapon weapon;
+
+   public Player(String name,int healthPercentage,Weapon weapon){
+       this.name=name;
+       this.weapon=weapon;
+       if (healthPercentage>100)
+           this.healthPercentage=100;
+       else this.healthPercentage = Math.max(healthPercentage,0);
+   }
+    public int healthRemaining(){
+       return healthPercentage;
+    }
+
+    public void loseHealth(int damage){
+       healthPercentage-=damage;
+       if(healthPercentage<=0) {
+           System.out.println(name + " player has been knocked out of game");
+           healthPercentage=0;
+       }
+   }
+
+   public void restoreHealth(int healthPotion){
+       healthPercentage+=healthPotion;
+       if (healthPercentage>100){
+           healthPercentage=100;
+       }
+   }
 }
--- src/main/java/org/example/Weapon.java
+++ src/main/java/org/example/Weapon.java
@@ -1,4 +1,22 @@
 package org.example;
 
 public enum Weapon {
+    SWORD(70,19);
+
+    private final int damage;
+
+    private final double attackSpeed;
+
+    Weapon(int damage, double attackSpeed){
+        this.damage=damage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public double getAttackSpeed() {
+        return attackSpeed;
+    }
 }
